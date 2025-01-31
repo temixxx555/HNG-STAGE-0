28cb28ebc61653d1b3cf4b3b400dca4b04dd4e46
Adebayo Temi <temiq3@gmail.com>
2025-01-31 11:49:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
   res.json({
     email: "temiq3@gmail.com",
     current_datetime: new Date().toISOString(),
-    github_url: "<https://github.com/temixxx555/HNG-STAGE-0>",
+    github_url: "https://github.com/temixxx555/HNG-STAGE-0",
   });
 });
 
